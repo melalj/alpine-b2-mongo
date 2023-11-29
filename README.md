d99cecd819e162a6d56dd553145d9b31bb12f21b
Simo Elalj <simo@elalj.com>
2023-11-29 23:43:26 +0100

    feat: Added Backup and Restore scripts

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # alpine-b2-mongo
-Alpine docker image with b2 and mongodb-tools
+
+Alpine docker image with b2 and mongodb-tools with two scripts:
+
+- `./backup.sh`: Backup a mongo database
+- `./restore.sh`: Restore a mongo database
+
+```sh
+# Edit your .env
+
+# Backup
+docker run -it --rm --env-file .env $(docker build -q .) sh /backup.sh
+
+# Restore
+docker run -it --rm --env-file .env $(docker build -q .) sh /restore.sh
+```
---
+++ restore.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+required_vars="B2_ACCOUNT_ID B2_ACCESS_KEY RESTORE_DB_NAME RESTORE_MONGODB_URI RESTORE_AUTH_DB_NAME RESTORE_ARCHIVE_NAME"
+
+for var in $required_vars; do
+	if [ -z "$(eval echo \$$var)" ]; then
+		echo "Error: Environment variable $var is not set."
+		exit 1
+	fi
+done
+
+echo "Authorizing B2 account"
+/usr/bin/b2 authorize-account $B2_ACCOUNT_ID $B2_ACCESS_KEY
+
+echo "Download $RESTORE_ARCHIVE_NAME from B2 bucket..."
+/usr/bin/b2 download-file-by-name --noProgress $B2_BUCKET $RESTORE_ARCHIVE_NAME $RESTORE_ARCHIVE_NAME
+
+echo "Restore MongoDB database $RESTORE_DB_NAME from compressed archive..."
+/usr/bin/mongorestore $RESTORE_EXTRA_PARAMS \
+	--uri "$RESTORE_MONGODB_URI" $RESTORE_EXTRA_FLAGS \
+	--authenticationDatabase "$RESTORE_AUTH_DB_NAME" \
+	--db "$RESTORE_DB_NAME" \
+	--gzip \
+	--archive="$RESTORE_ARCHIVE_NAME"
+
+echo "Cleaning up compressed archive..."
+rm "$RESTORE_ARCHIVE_NAME"
+
+echo "Restore complete!"
+exit 0
